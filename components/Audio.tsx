4bd144892b69d522e150bbe5b4d745ae8bb19955
Willem Liu <willem_liu@hotmail.com>
2019-08-01 11:47:41 +0200

    Audio player now fixed position
    Only shows when an actual source is present

--- components/Audio.tsx
+++ components/Audio.tsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import AudioStore from '../stores/Audio';
+import styled from 'styled-components';
 
 export default () => {
     const [audio, setAudio] = useState('');
@@ -11,9 +12,20 @@ export default () => {
         });
     }, []);
 
-    return (
-        <>
+    return audio ? (
+        <StyledAudio>
             <audio controls src={audio} />
-        </>
-    );
+        </StyledAudio>
+    ) : null;
 };
+
+const StyledAudio = styled.div`
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    display: flex;
+    audio {
+        flex: 1 1 auto;
+    }
+`;
