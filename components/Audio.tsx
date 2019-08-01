223a43807e66833186309d46bfb69061f82d205e
Willem Liu <willem_liu@hotmail.com>
2019-08-01 13:17:43 +0200

    Styled player

--- components/Audio.tsx
+++ components/Audio.tsx
@@ -34,15 +34,9 @@ export default () => {
                     <p>{audio.playerview.shareDescription}</p>
                 </div>
                 <div onClick={handleClick}>
-                    {paused ? (
-                        'Paused!!!'
-                    ) : (
-                        <span className="play-button">
-                            <span className="icon">
-                                <span className="icon-play" />
-                            </span>
-                        </span>
-                    )}
+                    <button
+                        className={`media-button${paused ? '' : ' pause'}`}
+                    ></button>
                 </div>
             </div>
         </StyledAudio>
@@ -76,12 +70,12 @@ const StyledAudio = styled.div`
                 font-family: Arial, Helvetica, sans-serif;
                 filter: invert(1);
                 margin: 0;
-            }
-            p {
-                font-size: 0.8rem;
                 text-overflow: ellipsis;
                 white-space: nowrap;
                 overflow: hidden;
+            }
+            p {
+                font-size: 0.8rem;
             }
 
             & + div {
@@ -91,6 +85,32 @@ const StyledAudio = styled.div`
                 margin: 0;
                 cursor: pointer;
             }
+        }
+    }
+
+    .media-button {
+        border: 0;
+        background: transparent;
+        box-sizing: border-box;
+        width: 0;
+        height: 2rem;
+        outline: none;
+
+        border-color: transparent transparent transparent #ffd200;
+        transition: 100ms all ease;
+        cursor: pointer;
+
+        // play state
+        border-style: solid;
+        border-width: 1rem 0 1rem 2rem;
+
+        &.pause {
+            border-style: double;
+            border-width: 0px 0 0px 2rem;
+        }
+
+        &:hover {
+            border-color: transparent transparent transparent #ffd200;
         }
     }
 `;
