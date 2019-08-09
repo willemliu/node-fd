3f611e96342e212854b81396c7f14c1b00c67fe1
Willem Liu <willem_liu@hotmail.com>
2019-08-09 20:51:55 +0200

    mongo

--- graphql/resolvers/validateToken.ts
+++ graphql/resolvers/validateToken.ts
@@ -10,42 +10,65 @@ export async function validateToken(
     return await db(args.token);
 }
 
+// async function db(token: string) {
+//     const mongoClient = mongodb.MongoClient;
+//     const dbName = 'bnr';
+//     const collection = 'tokens';
+//     const url = `mongodb+srv://${encodeURIComponent(
+//         'willem_liu@hotmail.com'
+//     )}:${encodeURIComponent(
+//         process.env.MONGO_DB_PASS || ''
+//     )}@cluster0-ucyju.mongodb.net/testgraphql?retryWrites=true&w=majority`;
+//     console.log(url);
+//     let result: boolean = false;
+//     let clientCon;
+//     try {
+//         clientCon = await mongoClient.connect(url, {
+//             useNewUrlParser: true,
+//             ssl: false,
+//             autoReconnect: true,
+//             auth: {
+//                 password: process.env.MONGO_DB_PASS || '',
+//                 user: 'willem_liu@hotmail.com',
+//             },
+//         });
+//         const db: Db = clientCon.db(dbName);
+//         // We would do a findOneAndDelete normally. But for development findOne is easier.
+//         const cursor = await db.collection(collection).findOne({ token });
+//         if (cursor && cursor.token) {
+//             console.log('token found:', cursor.token);
+//             result = true;
+//         }
+//     } catch (e) {
+//         console.error(e);
+//     } finally {
+//         if (clientCon) {
+//             clientCon.close();
+//         }
+//         console.log('finally false...');
+//         return result;
+//     }
+// }
+
 async function db(token: string) {
-    const mongoClient = mongodb.MongoClient;
-    const dbName = 'bnr';
-    const collection = 'tokens';
-    const url = `mongodb+srv://${encodeURIComponent(
-        'willem_liu@hotmail.com'
+    let result = false;
+    const MongoClient = mongodb.MongoClient;
+    const uri = `mongodb+srv://${encodeURIComponent(
+        'willem_liu'
     )}:${encodeURIComponent(
         process.env.MONGO_DB_PASS || ''
     )}@cluster0-ucyju.mongodb.net/testgraphql?retryWrites=true&w=majority`;
-    console.log(url);
-    let result: boolean = false;
-    let clientCon;
-    try {
-        clientCon = await mongoClient.connect(url, {
-            useNewUrlParser: true,
-            ssl: false,
-            autoReconnect: true,
-            auth: {
-                password: process.env.MONGO_DB_PASS || '',
-                user: 'willem_liu@hotmail.com',
-            },
-        });
-        const db: Db = clientCon.db(dbName);
-        // We would do a findOneAndDelete normally. But for development findOne is easier.
-        const cursor = await db.collection(collection).findOne({ token });
+    const client = new MongoClient(uri, { useNewUrlParser: true });
+    client.connect(async (err) => {
+        const collection = client.db('testgraphql').collection('bnr');
+        // perform actions on the collection object
+        const cursor = await collection.findOne({ token });
         if (cursor && cursor.token) {
             console.log('token found:', cursor.token);
             result = true;
         }
-    } catch (e) {
-        console.error(e);
-    } finally {
-        if (clientCon) {
-            clientCon.close();
-        }
-        console.log('finally false...');
-        return result;
-    }
+
+        client.close();
+    });
+    return result;
 }
