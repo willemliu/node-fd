75d58ca0d241ab78927dadc27502b564b9c5f22b
Willem Liu <willem_liu@hotmail.com>
2019-08-09 21:22:01 +0200

    mongo connection

--- graphql/resolvers/validateToken.ts
+++ graphql/resolvers/validateToken.ts
@@ -14,31 +14,30 @@ async function db(token: string) {
     const mongoClient = mongodb.MongoClient;
     const dbName = 'bnr';
     const collection = 'tokens';
-    const url = `mongodb+srv://${encodeURIComponent(
-        'willem_liu'
-    )}:${encodeURIComponent(
-        process.env.MONGO_DB_PASS || ''
-    )}@cluster0-ucyju.mongodb.net/testgraphql?retryWrites=true&w=majority`;
+    const url =
+        'mongodb+srv://app:' +
+        encodeURIComponent(process.env.MONGO_DB_PASS || '') +
+        '@cluster0-ucyju.mongodb.net/test?retryWrites=true&w=majority';
     console.log(url);
     let result: boolean = false;
-    let clientCon = await mongoClient.connect(url, {
-        useNewUrlParser: true,
-        ssl: false,
-        autoReconnect: true,
-        auth: {
-            password: process.env.MONGO_DB_PASS || '',
-            user: 'willem_liu',
-        },
-    });
-    const db: Db = clientCon.db(dbName);
-    // We would do a findOneAndDelete normally. But for development findOne is easier.
-    const cursor = await db.collection(collection).findOne({ token });
-    if (cursor && cursor.token) {
-        console.log('token found:', cursor.token);
-        result = true;
+    let clientCon;
+    try {
+        clientCon = await mongoClient.connect(url, {
+            useNewUrlParser: true,
+        });
+        const db: Db = clientCon.db(dbName);
+        // We would do a findOneAndDelete normally. But for development findOne is easier.
+        const cursor = await db.collection(collection).findOne({ token });
+        if (cursor && cursor.token) {
+            console.log('token found:', cursor.token);
+            result = true;
+        }
+    } catch (e) {
+        console.error(e);
+    } finally {
+        if (clientCon) {
+            clientCon.close();
+        }
+        return result;
     }
-    if (clientCon) {
-        clientCon.close();
-    }
-    return result;
 }
